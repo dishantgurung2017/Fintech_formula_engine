7270f7d4078d01c4ebd031861662fbc8cf0f2e29
Dishant Gurung <159797664+dishantgurung2017@users.noreply.github.com>
2026-01-02 12:29:04 +0530

    Add discount function to calculate employment benefits

--- functions.py
+++ functions.py
@@ -3,3 +3,8 @@ def delinquency_count(inputs):
 
 def debt_to_income(inputs):
     return inputs['debt'] / inputs['income']
+
+def discount(inputs):
+    if inputs['employment_type'] == 'MNC':
+        return 100
+    else: return 0
